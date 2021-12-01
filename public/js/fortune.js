ee96a747a1ab2c4e94908ced791737511db780ec
ProfSucrose <dr.blobbles@gmail.com>
2021-11-30 22:08:00 -0800

    changed profile picture size

---
+++ public/js/fortune.js
@@ -0,0 +1,675 @@
+const FORTUNES = `
+
+A clash of doctrine is not a disaster -- it is an opportunity.
+%
+A dream will always triumph over reality, once it is given the chance.
+		-- Stanislaw Lem
+%
+A lifetime isn't nearly long enough to figure out what it's all about.
+%
+A man who cannot seduce men cannot save them either.
+		-- Soren Kierkegaard
+%
+All hope abandon, ye who enter here!
+		-- Dante Alighieri
+%
+An idea is not responsible for the people who believe in it.
+%
+At the end of your life there'll be a good rest, and no further activities
+are scheduled.
+%
+Beauty is one of the rare things which does not lead to doubt of God.
+		-- Jean Anouilh
+%
+Catharsis is something I associate with pornography and crossword puzzles.
+		-- Howard Chaykin
+%
+Chance is perhaps the work of God when He did not want to sign.
+		-- Anatole France
+%
+Circumstances rule men; men do not rule circumstances.
+		-- Herodotus
+%
+Coincidences are spiritual puns.
+		-- G. K. Chesterton
+%
+Death is a spirit leaving a body, sort of like a shell leaving the nut behind.
+		-- Erma Bombeck
+%
+Death is God's way of telling you not to be such a wise guy.
+%
+Death is life's way of telling you you've been fired.
+		-- R. Geis
+%
+Death is Nature's way of recycling human beings.
+%
+Death is nature's way of saying 'Howdy'.
+%
+Death is nature's way of telling you to slow down.
+%
+Death is only a state of mind.
+
+Only it doesn't leave you much time to think about anything else.
+%
+Depart not from the path which fate has assigned you.
+%
+Depend on the rabbit's foot if you will, but remember, it didn't help
+the rabbit.
+		-- R. E. Shay
+%
+Disease can be cured; fate is incurable.
+		-- Chinese proverb
+%
+Ditat Deus.
+	[God enriches]
+%
+Do not believe in miracles -- rely on them.
+%
+Do not take life too seriously; you will never get out of it alive.
+%
+Do what you can to prolong your life, in the hope that someday you'll
+learn what it's for.
+%
+	"Do you think there's a God?"
+	"Well, ____SOMEbody's out to get me!"
+		-- Calvin and Hobbs
+%
+Do your part to help preserve life on Earth -- by trying to preserve your own.
+%
+Don't abandon hope.  Your Captain Midnight decoder ring arrives tomorrow.
+%
+Don't abandon hope: your Tom Mix decoder ring arrives tomorrow.
+%
+Don't go to bed with no price on your head.
+		-- Baretta
+%
+Don't have good ideas if you aren't willing to be responsible for them.
+%
+Don't kid yourself.  Little is relevant, and nothing lasts forever.
+%
+Don't let people drive you crazy when you know it's in walking distance.
+%
+Don't make a big deal out of everything; just deal with everything.
+%
+Don't stop to stomp ants when the elephants are stampeding.
+%
+Don't take life seriously, you'll never get out alive.
+%
+Down with categorical imperative!
+%
+Due to circumstances beyond your control, you are master of your fate
+and captain of your soul.
+%
+Dying is one of the few things that can be done as easily lying down.
+		-- Woody Allen
+%
+Each man is his own prisoner, in solitary confinement for life.
+%
+Each of us bears his own Hell.
+		-- Publius Vergilius Maro (Virgil)
+%
+Either I'm dead or my watch has stopped.
+		-- Groucho Marx's last words
+%
+Even the best of friends cannot attend each other's funeral.
+		-- Kehlog Albran, "The Profit"
+%
+Everything ends badly.  Otherwise it wouldn't end.
+%
+Everything in this book may be wrong.
+		-- Messiah's Handbook : Reminders for the Advanced Soul
+%
+Everything is possible.  Pass the word.
+		-- Rita Mae Brown, "Six of One"
+%
+Execute every act of thy life as though it were thy last.
+		-- Marcus Aurelius
+%
+Expansion means complexity; and complexity decay.
+%
+Facts are the enemy of truth.
+		-- Don Quixote
+%
+Fain would I climb, yet fear I to fall.
+		-- Sir Walter Raleigh
+%
+Faith goes out through the window when beauty comes in at the door.
+%
+Faith is under the left nipple.
+		-- Martin Luther
+%
+Fill what's empty, empty what's full, scratch where it itches.
+		-- Alice Roosevelt Longworth
+%
+For fast-acting relief, try slowing down.
+%
+For good, return good.
+For evil, return justice.
+%
+For your penance, say five Hail Marys and one loud BLAH!
+%
+Force has no place where there is need of skill.
+		-- Herodotus
+%
+FORTUNE'S RULES TO LIVE BY: #2
+	Never goose a wolverine.
+%
+FORTUNE'S RULES TO LIVE BY: #23
+	Don't cut off a police car when making an illegal U-turn.
+%
+From listening comes wisdom and from speaking repentance.
+%
+From the cradle to the coffin underwear comes first.
+		-- Bertolt Brecht
+%
+Getting into trouble is easy.
+		-- D. Winkel and F. Prosser
+%
+Getting there is only half as far as getting there and back.
+%
+Given a choice between grief and nothing, I'd choose grief.
+		-- William Faulkner
+%
+God instructs the heart, not by ideas, but by pains and contradictions.
+		-- De Caussade
+%
+God is the tangential point between zero and infinity.
+		-- Alfred Jarry
+%
+God made everything out of nothing, but the nothingness shows through.
+		-- Paul Valery
+%
+Good-bye.  I am leaving because I am bored.
+		-- George Saunders' dying words
+%
+Goodbye, cool world.
+%
+Got a dictionary?  I want to know the meaning of life.
+%
+Great acts are made up of small deeds.
+		-- Lao Tsu
+%
+Happiness is having a scratch for every itch.
+		-- Ogden Nash
+%
+Happiness is just an illusion, filled with sadness and confusion.
+%
+Happiness isn't having what you want, it's wanting what you have.
+%
+Happiness isn't something you experience; it's something you remember.
+		-- Oscar Levant
+%
+Having the fewest wants, I am nearest to the gods.
+		-- Socrates
+%
+He is truly wise who gains wisdom from another's mishap.
+%
+He knows not how to know who knows not also how to unknow.
+		-- Sir Richard Burton
+%
+He that composes himself is wiser than he that composes a book.
+		-- B. Franklin
+%
+He who knows others is wise.
+He who knows himself is enlightened.
+		-- Lao Tsu
+%
+He who knows that enough is enough will always have enough.
+		-- Lao Tsu
+%
+He who knows, does not speak.  He who speaks, does not know.
+		-- Lao Tsu
+%
+Here is a test to find whether your mission on earth is finished:
+if you're alive, it isn't.
+%
+I am not afraid of tomorrow, for I have seen yesterday and I love today.
+		-- William Allen White
+%
+I hate dying.
+		-- Dave Johnson
+%
+I have often regretted my speech, never my silence.
+		-- Publilius Syrus
+%
+I hope you're not pretending to be evil while secretly being good.
+That would be dishonest.
+%
+I just forgot my whole philosophy of life!!!
+%
+If a man has a strong faith he can indulge in the luxury of skepticism.
+		-- Friedrich Nietzsche
+%
+If the master dies and the disciple grieves, the lives of both have
+been wasted.
+%
+If the path be beautiful, let us not ask where it leads.
+		-- Anatole France
+%
+If we do not change our direction we are likely to end up where we are headed.
+%
+If we don't survive, we don't do anything else.
+		-- John Sinclair
+%
+If you can survive death, you can probably survive anything.
+%
+If you find a solution and become attached to it, the solution may become
+your next problem.
+%
+If you fool around with something long enough, it will eventually break.
+%
+If you have to hate, hate gently.
+%
+If you have to think twice about it, you're wrong.
+%
+If you keep anything long enough, you can throw it away.
+%
+If you only have a hammer, you tend to see every problem as a nail.
+		-- Maslow
+%
+If you put it off long enough, it might go away.
+%
+If you refuse to accept anything but the best you very often get it.
+%
+If you want divine justice, die.
+		-- Nick Seldon
+%
+If your aim in life is nothing, you can't miss.
+%
+Illusion is the first of all pleasures.
+		-- Voltaire
+%
+Immortality -- a fate worse than death.
+		-- Edgar A. Shoaff
+%
+In order to discover who you are, first learn who everybody else is;
+you're what's left.
+%
+In spite of everything, I still believe that people are good at heart.
+		-- Anne Frank
+%
+In the long run we are all dead.
+		-- John Maynard Keynes
+%
+In the next world, you're on your own.
+%
+Instead of loving your enemies, treat your friends a little better.
+		-- Edgar W. Howe
+%
+Intellect annuls Fate.
+So far as a man thinks, he is free.
+		-- Ralph Waldo Emerson
+%
+It does not do to leave a live dragon out of your calculations.
+%
+It is Fortune, not Wisdom, that rules man's life.
+%
+It's easier to take it apart than to put it back together.
+		-- Washlesky
+%
+It's hard to drive at the limit, but it's harder to know where the limits are.
+		-- Stirling Moss
+%
+It's not reality that's important, but how you perceive things.
+%
+	"It's today!" said Piglet.
+	"My favorite day," said Pooh.
+%
+Just remember, wherever you go, there you are.
+		-- Buckaroo Bonzai
+%
+Kindness is the beginning of cruelty.
+		-- Muad'dib [Frank Herbert, "Dune"]
+%
+Let us not look back in anger or forward in fear, but around us in awareness.
+		-- James Thurber
+%
+Life can be so tragic -- you're here today and here tomorrow.
+%
+Life exists for no known purpose.
+%
+Life is a grand adventure -- or it is nothing.
+		-- Helen Keller
+%
+Life is knowing how far to go without crossing the line.
+%
+Life is like a 10 speed bicycle.  Most of us have gears we never use.
+		-- C. Schultz
+%
+Life is like a sewer.  What you get out of it depends on what you put into it.
+		-- Tom Lehrer
+%
+Life is the childhood of our immortality.
+		-- Goethe
+%
+Life is the living you do, Death is the living you don't do.
+		-- Joseph Pintauro
+%
+Life is the urge to ecstasy.
+%
+Life may have no meaning, or, even worse, it may have a meaning of which
+you disapprove.
+%
+Life sucks, but death doesn't put out at all.
+		-- Thomas J. Kopp
+%
+Loneliness is a terrible price to pay for independence.
+%
+Man's reach must exceed his grasp, for why else the heavens?
+%
+Murphy was an optimist.
+%
+Murphy's Law is recursive.  Washing your car to make it rain doesn't work.
+%
+Music in the soul can be heard by the universe.
+		-- Lao Tsu
+%
+My theology, briefly, is that the universe was dictated but not signed.
+		-- Christopher Morley
+%
+Ninety percent of everything is crap.
+		-- Theodore Sturgeon
+%
+No matter where I go, the place is always called "here".
+%
+No use getting too involved in life -- you're only here for a limited time.
+%
+Nobody ever ruined their eyesight by looking at the bright side of something.
+%
+Nonsense and beauty have close connections.
+		-- E. M. Forster
+%
+Normal times may possibly be over forever.
+%
+Not every question deserves an answer.
+%
+Nothing in life is to be feared.  It is only to be understood.
+%
+Nothing is but what is not.
+%
+Nothing is ever a total loss; it can always serve as a bad example.
+%
+Nothing is so firmly believed as that which we least know.
+		-- Michel de Montaigne
+%
+Nothing matters very much, and few things matter at all.
+		-- Arthur Balfour
+%
+Once the toothpaste is out of the tube, it's hard to get it back in.
+		-- H. R. Haldeman
+%
+Once you've tried to change the world you find it's a whole bunch easier
+to change your mind.
+%
+One learns to itch where one can scratch.
+		-- Ernest Bramah
+%
+One meets his destiny often on the road he takes to avoid it.
+%
+Only that in you which is me can hear what I'm saying.
+		-- Baba Ram Dass
+%
+Paradise is exactly like where you are right now ... only much, much better.
+		-- Laurie Anderson
+%
+Perhaps the biggest disappointments were the ones you expected anyway.
+%
+Philosophy will clip an angel's wings.
+		-- John Keats
+%
+Push where it gives and scratch where it itches.
+%
+Reality always seems harsher in the early morning.
+%
+Reality does not exist -- yet.
+%
+Reality is bad enough, why should I tell the truth?
+		-- Patrick Sky
+%
+Reality is for people who lack imagination.
+%
+Reality is just a convenient measure of complexity.
+		-- Alvy Ray Smith
+%
+Reality is just a crutch for people who can't handle science fiction.
+%
+Reality is nothing but a collective hunch.
+		-- Lily Tomlin
+%
+"Reality is that which, when you stop believing in it, doesn't go away".
+		-- Philip K. Dick
+%
+Rule of Life #1 -- Never get separated from your luggage.
+%
+Seeing is believing.  You wouldn't have seen it if you hadn't believed it.
+%
+So little time, so little to do.
+		-- Oscar Levant
+%
+Sometimes even to live is an act of courage.
+		-- Seneca
+%
+Sometimes you get an almost irresistible urge to go on living.
+%
+That that is is that that is not is not.
+%
+The absurd is the essential concept and the first truth.
+		-- A. Camus
+%
+The best you get is an even break.
+		-- Franklin Adams
+%
+"The chain which can be yanked is not the eternal chain."
+		-- G. Fitch
+%
+The chief cause of problems is solutions.
+		-- Eric Sevareid
+%
+The chief danger in life is that you may take too many precautions.
+		-- Alfred Adler
+%
+The days are all empty and the nights are unreal.
+%
+The door is the key.
+%
+The farther you go, the less you know.
+		-- Lao Tsu, "Tao Te Ching"
+%
+The first requisite for immortality is death.
+		-- Stanislaw Lem
+%
+The greatest griefs are those we cause ourselves.
+		-- Sophocles
+%
+The major sin is the sin of being born.
+		-- Samuel Beckett
+%
+The more you complain, the longer God lets you live.
+%
+The moss on the tree does not fear the talons of the hawk.
+%
+The only difference between a rut and a grave is their dimensions.
+%
+The price of success in philosophy is triviality.
+		-- C. Glymour.
+%
+The questions remain the same.  The answers are eternally variable.
+%
+The savior becomes the victim.
+%
+The soul would have no rainbow had the eyes no tears.
+%
+The truth is rarely pure, and never simple.
+		-- Oscar Wilde
+%
+The truth is what is; what should be is a dirty lie.
+		-- Lenny Bruce
+%
+The truth of a thing is the feel of it, not the think of it.
+		-- Stanley Kubrick
+%
+The truth you speak has no past and no future.  It is, and that's all it
+needs to be.
+%
+There are no accidents whatsoever in the universe.
+		-- Baba Ram Dass
+%
+There are no winners in life, only survivors.
+%
+There is more to life than increasing its speed.
+		-- Mahatma Gandhi
+%
+There is no comfort without pain; thus we define salvation through suffering.
+		-- Cato
+%
+There is no cure for birth and death other than to enjoy the interval.
+		-- George Santayana
+%
+There is no sin but ignorance.
+		-- Christopher Marlowe
+%
+There's only one everything.
+%
+To give happiness is to deserve happiness.
+%
+To give of yourself, you must first know yourself.
+%
+To have died once is enough.
+		-- Publius Vergilius Maro (Virgil)
+%
+To lead people, you must follow behind.
+		-- Lao Tsu
+%
+Truth has no special time of its own.  Its hour is now -- always.
+		-- Albert Schweitzer
+%
+Truth is hard to find and harder to obscure.
+%
+Waste not fresh tears over old griefs.
+		-- Euripides
+%
+We can embody the truth, but we cannot know it.
+		-- Yates
+%
+We have nowhere else to go... this is all we have.
+		-- Margaret Mead
+%
+We have reason to be afraid.  This is a terrible place.
+		-- John Berryman
+%
+We're all in this alone.
+		-- Lily Tomlin
+%
+Well, you know, no matter where you go, there you are.
+		-- Buckaroo Banzai
+%
+What does not destroy me, makes me stronger.
+		-- Nietzsche
+%
+What makes the universe so hard to comprehend is that there's nothing
+to compare it with.
+%
+What sane person could live in this world and not be crazy?
+		-- Ursula K. LeGuin
+%
+What we Are is God's gift to us.
+What we Become is our gift to God.
+%
+Whatever occurs from love is always beyond good and evil.
+		-- Friedrich Nietzsche
+%
+Whatever you do will be insignificant, but it is very important that you do it.
+		-- Gandhi
+%
+When it's dark enough you can see the stars.
+		-- Ralph Waldo Emerson,
+%
+When the wind is great, bow before it;
+when the wind is heavy, yield to it.
+%
+When you die, you lose a very important part of your life.
+		-- Brooke Shields
+%
+Who does not trust enough will not be trusted.
+		-- Lao Tsu
+%
+Wisdom is knowing what to do with what you know.
+		-- J. Winter Smith
+%
+Wisdom is rarely found on the best-seller list.
+%
+With listening comes wisdom, with speaking repentance.
+%
+Yes, but which self do you want to be?
+%
+You can always pick up your needle and move to another groove.
+		-- Tim Leary
+%
+You can get *anywhere* in ten minutes if you drive fast enough.
+%
+You can never tell which way the train went by looking at the tracks.
+%
+You can no more win a war than you can win an earthquake.
+		-- Jeannette Rankin
+%
+You can observe a lot just by watching.
+		-- Yogi Berra
+%
+You can only live once, but if you do it right, once is enough.
+%
+You can't get there from here.
+%
+You can't mend a wristwatch while falling from an airplane.
+%
+You can't push on a string.
+%
+You can't take it with you -- especially when crossing a state line.
+%
+You have all eternity to be cautious in when you're dead.
+		-- Lois Platford
+%
+You will always find something in the last place you look.
+%
+Your happiness is intertwined with your outlook on life.
+%
+Your mind understands what you have been taught; your heart, what is true.
+%
+Your picture of the world often changes just before you get it into focus.
+%
+Your wig steers the gig.
+		-- Lord Buckley
+%
+You may be marching to the beat of a different drummer, but you're
+still in the parade.
+%
+The universe is made of stories, not of atoms.
+		-- Muriel Rukeyser
+%
+Freedom is what you do with what's been done to you.
+		-- Jean-Paul Sartre
+%
+There is a secret person undamaged within every individual.
+		-- Paul Shepard
+%
+We are governed not by armies and police but by ideas.
+		-- Mona Caird, 1892
+%
+You must be the change you wish to see in the world.
+		-- Mahatma Gandhi
+%
+The words fly away, the writings remain.
+%
+I am what you will be; I was what you are.
+%
+The people rule.
+%
+Perhaps the remembrance of these things will prove a source of future
+pleasure.
+		-- Virgil
+%
+Anyone who understands everything that comes out of fortune probably
+has a problem
+%
+If a million people say a foolish thing, it is still a foolish thing.
+		-- Anatole France
+`
