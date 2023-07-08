4f4544e5687b52e18c11a66f69336af045f790ce
ProfSucrose <kai@profsucrose.dev>
2023-07-08 00:32:29 -0400

    Tweak shader, add scenes/transitions

--- public/glsl/index/welcome.frag
+++ public/glsl/index/welcome.frag
@@ -1,8 +1,12 @@
 precision mediump float;
 
 uniform vec2 resolution;
-uniform float time;
+uniform float _time;
 uniform float fontSize;
+
+#define PI 3.14159
+
+float time;
 
 ivec2 char_a = ivec2(0x3f333300, 0xc1e3333);
 ivec2 char_b = ivec2(0x66663f00, 0x3f66663e);
@@ -93,12 +97,15 @@ ivec2 string(int i) {
     if (i == 45) return char_v;
     if (i == 46) return char_esc;
     if (i == 47) return char_space;
+    return char_space;
 }
 
 vec3 textBuffer(vec2 fragCoord) {
+    int y = (int(resolution.y) - int(fragCoord.y)) / int(fontSize);
+    fragCoord.x += sin(float(y) + time * 10.0) + time * 100.0 * sin(float(y));
     ivec2 coord = ivec2(int(fragCoord.x), int(fragCoord.y));
-    int y = (int(resolution.y) - coord.y) / int(fontSize);
-    int x = int(mod(float(coord.x / int(fontSize)) + time * mod(float(y), 2.0) * 2.0, 48.0));
+    // int x = int(mod(float(coord.x / int(fontSize)) - time * mod(float(y), 2.0) * 2.0, 48.0));
+    int x = int(mod(float(coord.x / int(fontSize)), 48.0));
     int charPixelX = int(mod(float(coord.x), fontSize) / (fontSize / 8.0));
     int charPixelY = int(mod(float(coord.y), fontSize) / (fontSize / 8.0));
    
@@ -107,13 +114,63 @@ vec3 textBuffer(vec2 fragCoord) {
     return col;
 }
 
-void main() {
-    vec2 sampleCoord = gl_FragCoord.xy;
-    sampleCoord.y /= mix(0.9, 2.0, (sin(sampleCoord.x / 300.0 + time / 2.0) + 1.0) / 2.0);
-    sampleCoord.x /= mix(0.9, 2.0, (sin(sampleCoord.y / 300.0 + time / 2.0) + 1.0) / 2.0);
-    sampleCoord.x += time * 20.;
+vec2 rotate(vec2 v, float a) {
+	float s = sin(a);
+	float c = cos(a);
+	mat2 m = mat2(c, -s, s, c);
+	return m * v;
+}
 
-    vec3 col = textBuffer(sampleCoord);
+vec3 polar(vec2 coord) {
+    vec2 center = resolution / 2.0;
+
+    vec2 diff = coord - center;
+
+    float lineLength = fontSize * 48.0;
+    float r = length(diff) * min(resolution.x, resolution.y) / lineLength;
+    float theta = atan(diff.y, diff.x) / (2.0 * PI) * lineLength * 2.0;
+
+    vec3 col = textBuffer(vec2(-theta, r));
+    return col;
+}
+
+vec3 rectangular(vec2 coord) {
+    coord.y /= mix(0.9, 2.0, (sin(coord.x / 300.0 + time / 2.0) + 1.0) / 2.0);
+    coord.x /= mix(0.9, 2.0, (sin(coord.y / 300.0 + time / 2.0) + 1.0) / 2.0);
+    coord.x += time * 20.;
+
+    vec3 col = textBuffer(coord);
+    return col;
+}
+
+vec3 matrix(vec2 _coord) {
+    ivec2 coord = ivec2(int(_coord.x), int(_coord.y));
+    int x = coord.x / int(fontSize);
+    float colHeight = sin(float(x)) * 50.0 + 60.0;
+    int y = int(mod(mod(float((int(resolution.y) - coord.y) / int(fontSize)), 50.0) - time * 10.0, colHeight));
+    int charPixelX = int(mod(float(coord.x), fontSize) / (fontSize / 8.0));
+    int charPixelY = int(mod(float(coord.y), fontSize) / (fontSize / 8.0));
+   
+    bool isFilled = char(string(y), charPixelX, charPixelY);
+    vec3 col = isFilled ? vec3(float(y > 30 + int(3.0 * sin(float(x)))) * 0.4 + 0.6) : vec3(0.0); // black if filled, white otherwise
+    return col;
+}
+
+#define TRANSITIONS_WINDOW 2.0
+
+void main() {
+    time = _time * 0.1;
+    vec2 sampleCoord = gl_FragCoord.xy;
+
+    vec3 col1 = polar(sampleCoord);
+    vec3 col2 = rectangular(sampleCoord);
+    vec3 col3 = matrix(sampleCoord);
+
+    float t = mod(time, TRANSITIONS_WINDOW)/TRANSITIONS_WINDOW;
+
+    vec3 col = mix(col1, col2, smoothstep(0.2, 0.3, t));
+    col = mix(col, col3, smoothstep(0.5, 0.6, t));
+    col = mix(col, col1, smoothstep(0.8, 0.9, t));
 
     vec2 uv = gl_FragCoord.xy / resolution.xy;
 	vec4 rainbow = 0.5+0.5*vec4(cos(time+uv.xyx+vec3(0, 2, 4)),1.0);
--- public/js/index/script.js
+++ public/js/index/script.js
@@ -6,135 +6,138 @@
  * @param {number} type Shader type
  * @return {Array} Status and error message (or null) of compilation
  */
- function compileAndAttachShader(gl, program, source, type) {
-	const shader = gl.createShader(type)
-	gl.shaderSource(shader, source)
-	gl.compileShader(shader)
+function compileAndAttachShader(gl, program, source, type) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
 
-	const status = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
-	if (!status) {
-		const error = gl.getShaderInfoLog(shader)
-		return [ status, error.slice(0, error.length - 2) ]
-	}
+    const status = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (!status) {
+        const error = gl.getShaderInfoLog(shader);
+        return [status, error.slice(0, error.length - 2)];
+    }
 
-	gl.attachShader(program, shader)
-	return [ true, null ]
+    gl.attachShader(program, shader);
+    return [true, null];
 }
 
-let fontSize = calcFontSize(innerWidth, innerHeight)
+let fontSize = calcFontSize(innerWidth, innerHeight);
 
 function calcFontSize(width, height) {
-    return Math.floor(Math.min(width, height) * 0.03)
+    return Math.floor(Math.min(width, height) * 0.03);
 }
 
 /**
  * Resizes DOM Element to window innerWidth and innerHeight
- * @param {DOMElement} element 
+ * @param {DOMElement} element
  */
 function resizeElementToWindow(element) {
-    fontSize = calcFontSize(innerWidth, innerHeight)
-    element.width = window.innerWidth - window.innerWidth % fontSize
-    element.height = window.innerHeight - window.innerHeight % fontSize
+    fontSize = calcFontSize(innerWidth, innerHeight);
+    element.width = window.innerWidth - (window.innerWidth % fontSize);
+    element.height = window.innerHeight - (window.innerHeight % fontSize);
 }
 
-const fortunes = FORTUNES.split('%')
+const fortunes = FORTUNES.split("%");
 
 /**
  * Fetches random UNIX fortune
  * @returns {String} fortune
  */
 function getFortune() {
-    return fortunes[Math.floor(Math.random() * fortunes.length)].trim()
+    return fortunes[Math.floor(Math.random() * fortunes.length)].trim();
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
     // load fortune
-    const fortune = document.querySelector('.fortune')
-    fortune.innerText = `${getFortune()}`
+    const fortune = document.querySelector(".fortune");
+    fortune.innerText = `${getFortune()}`;
 
     // load canvas
-    const canvas = document.querySelector('canvas')
+    const canvas = document.querySelector("canvas");
 
-    resizeElementToWindow(canvas)
-    window.addEventListener('resize', () => {
-        resizeElementToWindow(canvas)
-    })
-    
+    resizeElementToWindow(canvas);
+    window.addEventListener("resize", () => {
+        resizeElementToWindow(canvas);
+    });
+
     // load WebGL
-    const gl = canvas.getContext('webgl')
+    const gl = canvas.getContext("webgl");
     if (gl == null) {
-        alert('Your browser does not support WebGL; try switching or updating your browser!')
-        return
+        alert(
+            "Your browser does not support WebGL; try switching or updating your browser!"
+        );
+        return;
     }
 
-    const vertices = [
-        -1, -1, 0,
-        -1,  1, 0,
-         1, -1, 0,
-         1,  1, 0
-    ]
+    const vertices = [-1, -1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0];
 
-    const indices = [0, 1, 2, 1, 3, 2]
+    const indices = [0, 1, 2, 1, 3, 2];
 
-    const vertexBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
-    gl.bindBuffer(gl.ARRAY_BUFFER, null)
+    const vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-    const indexBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW)
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(
+        gl.ELEMENT_ARRAY_BUFFER,
+        new Uint16Array(indices),
+        gl.STATIC_DRAW
+    );
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)	
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 
     const vertexSource = `
         attribute vec3 coords;
         void main() {
             gl_Position = vec4(coords, 1.0);
         }
-    `
+    `;
 
     // import fragment shader from other JS
-    const fragmentSource = await (await fetch('glsl/index/welcome.frag')).text()
+    const fragmentSource = await (
+        await fetch("glsl/index/welcome.frag")
+    ).text();
 
-    let program = gl.createProgram()
+    let program = gl.createProgram();
 
-    compileAndAttachShader(gl, program, vertexSource, gl.VERTEX_SHADER)
-    compileAndAttachShader(gl, program, fragmentSource, gl.FRAGMENT_SHADER)
+    compileAndAttachShader(gl, program, vertexSource, gl.VERTEX_SHADER);
+    compileAndAttachShader(gl, program, fragmentSource, gl.FRAGMENT_SHADER);
 
-    gl.linkProgram(program)
-    gl.useProgram(program)
+    gl.linkProgram(program);
+    gl.useProgram(program);
 
-    const coord = gl.getAttribLocation(program, 'coords')
-    gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0)
-    gl.enableVertexAttribArray(coord)
+    const coord = gl.getAttribLocation(program, "coords");
+    gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(coord);
 
-    const fps = 80
+    const fps = 80;
 
-    let t = Math.random() * 1000
+    let t = Math.random() * 1000;
 
     const draw = () => {
-        gl.clearColor(0, 0, 0, 1)
-        gl.clear(gl.COLOR_BUFFER_BIT)
-        gl.viewport(0, 0, canvas.width, canvas.height)
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.viewport(0, 0, canvas.width, canvas.height);
 
-        const time = gl.getUniformLocation(program, 'time')
-        gl.uniform1f(time, t)
+        const time = gl.getUniformLocation(program, "_time");
+        gl.uniform1f(time, t);
 
-        const fontSizeLoc = gl.getUniformLocation(program, 'fontSize')
-        gl.uniform1f(fontSizeLoc, fontSize)
+        const fontSizeLoc = gl.getUniformLocation(program, "fontSize");
+        gl.uniform1f(fontSizeLoc, fontSize);
 
-        const resolution = gl.getUniformLocation(program, 'resolution')
-        gl.uniform2fv(resolution, [ canvas.width, canvas.height ])
+        const resolution = gl.getUniformLocation(program, "resolution");
+        gl.uniform2fv(resolution, [canvas.width, canvas.height]);
 
         gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
 
-        t += 0.01
+        t += 0.01;
 
-        setTimeout(() => window.requestAnimationFrame(draw), 1000/fps)
-    }
+        setTimeout(() => window.requestAnimationFrame(draw), 1000 / fps);
+    };
 
-    setTimeout(() => window.requestAnimationFrame(draw), 1000/fps)
-})
+    setTimeout(() => window.requestAnimationFrame(draw), 1000 / fps);
+});
